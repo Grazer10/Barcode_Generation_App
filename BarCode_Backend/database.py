f4bb99923d4585cac475c0b24607a99c41b7f138
Grazer10 <harishsubramaniam25@gmail.com>
2024-06-03 18:58:06 +0530

    Update database.py

--- BarCode_Backend/database.py
+++ BarCode_Backend/database.py
@@ -1,1 +1,11 @@
+import pymongo
+
+mongo_URI = "mongodb://localhost:27017"
+client = pymongo.MongoClient("mongodb://localhost:27017")
+
+print("database_client",client)
+
+db = client["bar_code_generate"]
+clone_bar_data = db["clone_bar_data"]
+series_bar_data = db["series_bar_data"]
 
